5089cc29d11fb116534f362b7b273fc93a0aed8c
Yanni <yanninurlaela@loangarage.com>
2013-09-03 10:19:10 +0700

    edited

--- baru.php
+++ baru.php
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-"<?php print 'another file';" 
+<?php print 'another file';
 echo "my test";
-=======
-<?php 
+
 print 'another file';
 print '\n';
 print 'another line';
 ?>
->>>>>>> remotegit/master
